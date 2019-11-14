2ec1bf3bd1c3d482cdab9919990225b50c48b20f
Moésio Filho <moesiof.dev@gmail.com>
2019-11-14 10:07:56 -0300

    v1.0 - Estrutura Básica

---
+++ app/src/main/java/com/moesiof/minhasolimpiadas_escola/glideconfig/MyAppGlideModule.kt
@@ -0,0 +1,7 @@
+package com.moesiof.minhasolimpiadas_escola.glideconfig
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class AppGlideModule : AppGlideModule()
